1c05158fd0d1d26250320b89a367bc0c248f9e24
Manuel Lorenzo Tallon <mlorenzotallon@gmail.com>
2024-11-20 16:53:34 +0100

    Added new characters

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -2,33 +2,44 @@ import { Injectable } from '@angular/core';
 // Importar el archivo JSON desde la carpeta assets/data
 import * as charactersData from '../../assets/data/characters.json';
 // Importar las interfaces desde models
-import { ICharactersByFloor, ICharacter } from '../models/character.model'; // Actualizado con la "I"
+import { ICharactersByFloor, ICharacter } from '../models/character.model'; // Modelo de datos para personajes
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
-  // Cargar los datos de los personajes desde el archivo JSON
+  // Datos de los personajes cargados desde el archivo JSON
   private characters: ICharactersByFloor = charactersData;
 
   constructor() {}
 
-  // Método para obtener el número total de personajes
+  /**
+   * Obtiene el número total de personajes disponibles en todos los pisos.
+   * @returns {number} Número total de personajes.
+   */
   getTotalCharacters(): number {
     let totalCharacters = 0;
+
+    // Iterar por cada piso en los datos de personajes
     for (const floor in this.characters) {
       if (
         Object.prototype.hasOwnProperty.call(this.characters, floor) &&
-        floor !== 'default'
+        floor !== 'default' // Ignorar cualquier clave predeterminada no relacionada con pisos.
       ) {
-        totalCharacters += this.characters[floor].length; // Sumar los personajes por piso
+        totalCharacters += this.characters[floor].length; // Sumar el número de personajes de cada piso.
       }
     }
+
     return totalCharacters;
   }
 
-  // Método para obtener los personajes de un piso específico
+  /**
+   * Obtiene los personajes de un piso específico por su nombre.
+   * @param {string} floor - Nombre del piso (por ejemplo, '1A', '2B').
+   * @returns {ICharacter[]} Lista de personajes que pertenecen al piso especificado.
+   */
   getCharactersByFloorName(floor: string): ICharacter[] {
-    return this.characters[floor] || []; // Si no se encuentra el piso, devuelve un array vacío
+    // Si el piso no existe en los datos, devolver un array vacío.
+    return this.characters[floor] || [];
   }
 }
